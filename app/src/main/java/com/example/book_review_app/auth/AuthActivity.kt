67c2a3911440157c6d2ec56387e2c45f1db50e5b
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-04-18 13:14:58 +0900

    feat: add auth form layout

--- app/src/main/java/com/example/book_review_app/auth/AuthActivity.kt
+++ app/src/main/java/com/example/book_review_app/auth/AuthActivity.kt
@@ -5,15 +5,22 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.book_review_app.databinding.ActivityAuthBinding
 
 open class AuthActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityAuthBinding.inflate(layoutInflater) }
     protected lateinit var emailEditText: EditText
     protected lateinit var passwordEditText: EditText
     protected lateinit var authButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+        emailEditText = binding.emailEditText
+        passwordEditText = binding.passwordEditText
+        authButton = binding.authButton
     }
 
     // This method should be overridden only in SignUpActivity
